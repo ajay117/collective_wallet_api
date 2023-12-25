5485bb9fc1e51e4aba8c124ffb9879d1dcb1fbe8
Ajay KC <meajay64@gmail.com>
2023-12-25 11:07:21 +0900

    Return only necessary properties as response

--- models/User.model.js
+++ models/User.model.js
@@ -15,6 +15,16 @@ const userSchema = new mongoose.Schema({
   adminInGroup: [], //We will store user id and usernane inside the array. There will be only one admin
 });
 
+userSchema.set("toJSON", {
+  transform: (doc, returnedDoc) => {
+    returnedDoc.id = returnedDoc._id.toString();
+    delete returnedDoc._id;
+    delete returnedDoc.__v;
+    delete returnedDoc.hashedPassword;
+    return returnedDoc;
+  },
+});
+
 userSchema.plugin(uniqueValidator);
 const User = mongoose.model("User", userSchema);
 
