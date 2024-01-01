2d2390693e9fdb82332748cd60d24b1f5b900aa8
Ajay KC <meajay64@gmail.com>
2024-01-01 22:09:48 +0900

    Write tests & implement routes to add and delete member

--- controllers/Member.controller.js
+++ controllers/Member.controller.js
@@ -25,7 +25,42 @@ router.post("/:id/members/:userId/add_member", authenticateToken, async (req, re
   }
   group.members.push(user._id);
   await group.save();
+
+  user.memberInGroup.push({
+    id: group.id,
+    groupName: group.groupName,
+  });
+  await user.save();
+
+  // console.log(group, user);
   res.status(200).json(group);
+});
+
+router.delete("/:id/members/:userId/delete_member", authenticateToken, async (req, res) => {
+  const groupId = req.params.id;
+  const userId = req.params.userId;
+
+  if (!(groupId && userId)) {
+    res.status(400).json({ message: "Please provide a valid group and member id" });
+    return;
+  }
+
+  const group = await Group.findOne({ _id: groupId });
+  const user = await User.findOne({ _id: userId });
+  const isAdmin = group.admin[0].id === req.user.user.id;
+
+  if (!isAdmin) {
+    res.status(401).json({ message: "You are not authorized" });
+    return;
+  }
+
+  group.members.pull({ _id: userId });
+  await group.save();
+
+  user.memberInGroup = user.memberInGroup.filter((obj) => obj.id !== groupId);
+  await user.save();
+
+  res.status(200).end();
 });
 
 module.exports = router;
--- tests/member_controller.test.js
+++ tests/member_controller.test.js
@@ -4,23 +4,26 @@ const Group = require("../models/Group.model");
 const app = require("../app");
 const supertest = require("supertest");
 const { createHashedPassword, groupsInDB, usersInDB } = require("../helpers/test.helper");
-const { application } = require("express");
 
 const api = supertest(app);
-
 let token;
+
+const userAfterPwdHashFromDb = async (userList, index, password) => {
+  userList[index].hashedPassword = await createHashedPassword(password);
+  let userObject = new User(userList[index]);
+  userObject = await userObject.save();
+  return userObject;
+};
 
 const initialData = [
   {
     username: "ajay",
-    password: "111111",
     memberInGroup: [],
     adminInGroup: [],
     id: "6588f25bec299c11ec41db2f",
   },
   {
     username: "bijay",
-    password: "111111",
     memberInGroup: [],
     adminInGroup: [],
     id: "65893f104d6fb5608c9b1309",
@@ -44,13 +47,8 @@ const initialDataGroups = [
 
 beforeEach(async () => {
   await User.deleteMany();
-  let password = "111111";
-  initialData[0].hashedPassword = await createHashedPassword(password);
-  let userObject1 = new User(initialData[0]);
-  await userObject1.save();
-  initialData[1].hashedPassword = await createHashedPassword(password);
-  let userObject2 = new User(initialData[1]);
-  await userObject2.save();
+  let userObject1 = await userAfterPwdHashFromDb(initialData, 0, "111111");
+  let userObject2 = await userAfterPwdHashFromDb(initialData, 1, "111111");
 
   const userCredentials = {
     username: "ajay",
@@ -65,47 +63,57 @@ beforeEach(async () => {
 
   token = body.token;
 
-  const allUsersInDb = await usersInDB();
-  const secondUserInDb = allUsersInDb[1];
-
   await Group.deleteMany();
-  const { id, user } = body.user;
-  initialDataGroups[0].admin.push({ id, user });
 
+  //Setting first group with first user as admin
+  initialDataGroups[0].admin.push({
+    id: userObject1.id,
+    username: userObject1.username,
+  });
+  let group1 = new Group(initialDataGroups[0]);
+  await group1.save();
 
-  //creating second user from db to push it to property admin in second group
-  const secondUser = {
-    id: secondUserInDb.id,
-    username: secondUserInDb.username,
-  };
-  initialDataGroups[1].admin.push(secondUser);
+  userObject1.adminInGroup.push({
+    id: group1.id,
+    groupName: group1.groupName,
+  });
+  await userObject1.save();
 
-  let groupObject = new Group(initialDataGroups[0]);
-  await groupObject.save();
-  groupObject = new Group(initialDataGroups[1]);
-  await groupObject.save();
+  //Creating second group with second user as admin
+  initialDataGroups[1].admin.push({
+    id: userObject2.id,
+    username: userObject2.username,
+  });
+  let group2 = new Group(initialDataGroups[1]);
+  await group2.save();
+
+  userObject2.adminInGroup.push({
+    id: group2.id,
+    groupName: group2.groupName,
+  });
+  await userObject2.save();
 });
 
-describe("test routes with /groups/:id/members/ ", () => {
-//   test("request made by not admin will return 401", async () => {
-//     const allUsersInDb = await usersInDB();
-//     const allGroupsInDb = await groupsInDB();
-//     const firstUserInDb = allUsersInDb[0];
-//     const secondGroupInDb = allGroupsInDb[1];
-
-//     console.log({ firstUserInDb, secondGroupInDb });
-
-//     await api
-//       .post(`/groups/${secondGroupInDb.id}/members/${firstUserInDb.id}/add_member`)
-//       .set("Authorization", `Bearer {token}`)
-//       .expect(401)
-//       .expect("Content-Type", /application\/json/);
-//   });
-
-  test("post request to /groups/:id/members/:userId/add_member will add a member to the group", async () => {
+describe("request starting with /groups/:id/members/ ", () => {
+  test("request made by not admin will return 401", async () => {
     const allUsersInDb = await usersInDB();
     const allGroupsInDb = await groupsInDB();
-    const secondUserInDb = allUsersInDb[1];
+    const firstUserInDb = allUsersInDb[0];
+    const secondGroupInDb = allGroupsInDb[1];
+
+    console.log({ firstUserInDb, secondGroupInDb });
+
+    await api
+      .post(`/groups/${secondGroupInDb.id}/members/${firstUserInDb.id}/add_member`)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(401)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("post request to /groups/:id/members/:userId/add_member, by admin will add a member to the group", async () => {
+    const allUsersInDb = await usersInDB();
+    const allGroupsInDb = await groupsInDB();
+    let secondUserInDb = allUsersInDb[1];
     const firstGroupInDb = allGroupsInDb[0];
 
     const { body } = await api
@@ -114,7 +122,37 @@ describe("test routes with /groups/:id/members/ ", () => {
       .expect(200)
       .expect("Content-Type", /application\/json/);
 
+    const allUsersAfterRequestInDb = await usersInDB();
+    secondUserInDb = allUsersAfterRequestInDb[1];
+    const memberInGroup = secondUserInDb.memberInGroup.map((obj) => obj.id);
+
     expect(body.members).toContain(secondUserInDb.id);
+    expect(memberInGroup).toContain(body.id);
+  });
+
+  test("request to remove a member  from the group by admin should return 200", async () => {
+    const allUsersInDb = await usersInDB();
+    const allGroupsInDb = await groupsInDB();
+    const firstGroupInDb = allGroupsInDb[0];
+    const secondUserInDb = allUsersInDb[1];
+
+    //Adding second member from db to first group in db by admin as a member
+    await api
+      .post(`/groups/${firstGroupInDb.id}/members/${secondUserInDb.id}/add_member`)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    //Removing second member from first group as a member by admin
+    await api
+      .delete(`/groups/${firstGroupInDb.id}/members/${secondUserInDb.id}/delete_member`)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(200);
+
+    // await api
+    //   .del("/groups/:groupId/members/:memberId/delete_member")
+    //   .set("Authorization", `Bearer ${token}`)
+    //   .expect(200);
   });
 });
 
